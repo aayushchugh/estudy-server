b8d5607b2188eae1b469156e6e5001aed7855df8
Ayush Chugh <ayushchugh2006@gmail.com>
2021-09-26 14:09:02 +0530

    delete user

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,7 @@
 import userModel from '../models/userModel.js';
 
+/* ------------------------------ get all users ----------------------------- */
+
 export async function getAllUsers(req, res) {
 	try {
 		const users = await userModel.find();
@@ -7,6 +9,25 @@ export async function getAllUsers(req, res) {
 		res.send({
 			status: 200,
 			data: users,
+		});
+	} catch (err) {
+		res.send({
+			status: 500,
+			message: 'internal server error',
+		});
+	}
+}
+
+/* ------------------------------- delete user ------------------------------ */
+
+export async function deleteUser(req, res) {
+	try {
+		const { id } = req.params;
+		await userModel.findByIdAndDelete(id);
+
+		res.send({
+			status: 200,
+			message: 'user deleted successfully',
 		});
 	} catch (err) {
 		res.send({
