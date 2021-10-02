98dd8f26dd9d66cdbe63624f497ee01cc1892ac5
Ayush Chugh <ayushchugh2006@gmail.com>
2021-10-02 17:08:06 +0530

    get all classes

--- controllers/classController.js
+++ controllers/classController.js
@@ -1,19 +1,43 @@
 const Class = require('../models/classModel');
 
+/* ------------------------------ add new class ----------------------------- */
+
 exports.postNewClass = async function (req, res) {
 	try {
+		// get data from user
 		const { title, description } = req.body;
 
+		// create new class
 		const newClass = await Class.create({
 			title: title,
 			description: description,
 			subjects: [],
 		});
 
+		// send response
 		res.send({
 			status: 204,
 			message: 'Class created successfully',
 			data: newClass,
+		});
+	} catch (err) {
+		res.send({
+			status: 500,
+			message: 'internal server error',
+		});
+	}
+};
+
+/* ----------------------------- get all classes ---------------------------- */
+exports.getAllClasses = async function (req, res) {
+	try {
+		// get all classes
+		const classes = await Class.find();
+
+		// send response
+		res.send({
+			status: 200,
+			data: classes,
 		});
 	} catch (err) {
 		res.send({
--- routes/classRouter.js
+++ routes/classRouter.js
@@ -4,5 +4,6 @@ const classController = require('../controllers/classController');
 const router = express.Router();
 
 router.route('/class/add').post(classController.postNewClass);
+router.route('/class/all').get(classController.getAllClasses);
 
 module.exports = router;
